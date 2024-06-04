82ed23ca0532bb7a88d3455c443d7896eabd84b5
LuanGrod <luan14rodrigues17@gmail.com>
2024-06-03 22:05:40 -0300

    SqlServer service adicionado

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
+using api_todos.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<TodoContext> (options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
