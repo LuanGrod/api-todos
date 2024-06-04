15a900bec7dbf9b9c30628e733f2af1b834dbaf8
LuanGrod <luan14rodrigues17@gmail.com>
2024-06-03 23:46:58 -0300

    POST Create criado

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -1,10 +1,11 @@
 using api_todos.Context;
+using api_todos.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api_todos.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     public class TodoController : ControllerBase
     {
         private readonly TodoContext _context;
@@ -25,6 +26,16 @@ namespace api_todos.Controllers
         //GET ObterPorStatus
 
         //POST
+        [HttpPost]
+        public ActionResult Create(Todo todo)
+        {
+
+            _context.Todos.Add(todo);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(ObterPorId), new { id = todo.Id }, todo);
+
+        }
 
         //PUT Id
 
